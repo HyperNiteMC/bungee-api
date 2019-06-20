018e162df7bd820546926f82eda69e746ca6d61a
Lam <tsukiko852@gmail.com>
2019-06-20 14:29:58 +0800

    added sql statement builder, author XUAN

--- API/src/com/ericlam/mc/bungee/hnmc/builders/function/ChatRunner.java
+++ API/src/com/ericlam/mc/bungee/hnmc/builders/function/ChatRunner.java
@@ -4,6 +4,8 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 /**
  * @see com.ericlam.mc.bungee.hnmc.builders.MessageBuilder#run(ChatRunner)
+ * @see com.ericlam.mc.bungee.hnmc.builders.MessageBuilder#runClicks(int, ChatRunner)
+ * @see com.ericlam.mc.bungee.hnmc.builders.MessageBuilder#runTimeout(int, ChatRunner)
  */
 @FunctionalInterface
 public interface ChatRunner {
